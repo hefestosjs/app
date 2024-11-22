36e60486926a5b28210361e8f9e5ae61e0ba00d3
Lucas Nathan J. da Silva <lucasnathanj@gmail.com>
2024-11-22 16:30:57 -0300

    add env example

---
+++ README.md
@@ -0,0 +1,1 @@
+# Hefestos Application Template
--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["app/resources/**/*.nj"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("tailwindcss"), require("autoprefixer")],
-};
